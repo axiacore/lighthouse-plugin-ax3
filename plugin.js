49bacc282ddf8247e2e74ced9feea17075476d78
julian <julian.osorio@axicore.com>
2021-03-10 22:38:11 -0500

    line_space

--- plugin.js
+++ plugin.js
@@ -1,19 +1,18 @@
 module.exports = {
-    audits: [{path: 'lighthouse-plugin-ax3/audits/search-anchors.js'},
-
-             {path: 'lighthouse-plugin-ax3/audits/url-social.js'},
-
-             {path: 'lighthouse-plugin-ax3/audits/favicon-icon.js'}],
-
+    audits: [
+        {path: 'lighthouse-plugin-ax3/audits/search-anchors.js'},
+        {path: 'lighthouse-plugin-ax3/audits/url-social.js'},
+        {path: 'lighthouse-plugin-ax3/audits/favicon-icon.js'}
+    ],
     category: {
         title: 'AX3',
         description:
             'Lighthouse plugin to audit Axiacore projects',
-        auditRefs: [{id: 'search-anchors.js-id', weight: 1, group: 'links' },
-        
-                    {id: 'url-social.js-id', weight: 3, group: 'links'},
-
-                    {id: 'favicon-icon.js-id', weight: 1, group: 'image'}],
+        auditRefs: [
+            {id: 'search-anchors.js-id', weight: 1, group: 'links' },
+            {id: 'url-social.js-id', weight: 3, group: 'links'},
+            {id: 'favicon-icon.js-id', weight: 1, group: 'image'}
+        ],
     },
     groups: {
         'links': {
